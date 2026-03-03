5f8381f96059e63daba87f269c5231f9f4948006
M Bima J <bimamj7@gmail.com>
2026-03-03 21:23:50 +0700

    praktikum 6

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
