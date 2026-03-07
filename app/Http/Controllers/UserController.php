ff41fbee41f45c8ba7be00fbde5c08f1870b5dc3
M Bima J <bimamj7@gmail.com>
2026-03-07 21:01:59 +0700

    js4 praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,20 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+
+        // $user = UserModel::firstWhere('level_id',1);
+        // $user = UserModel::find(1);
         return view('user', ['data' => $user]);
     }
 }
