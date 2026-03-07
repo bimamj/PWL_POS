cfa1b2b084e7a0915c136fecf5d1db30f8fe6e31
M Bima J <bimamj7@gmail.com>
2026-03-07 21:12:17 +0700

    js4 prakt 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,12 +18,14 @@ class UserController extends Controller
         // ];
         // UserModel::create($data);
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
 
         // $user = UserModel::firstWhere('level_id',1);
         // $user = UserModel::find(1);
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
