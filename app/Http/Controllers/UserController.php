966b56a238de37860b0c99c1bdcf134e1643f8a9
M Bima J <bimamj7@gmail.com>
2026-03-10 22:43:44 +0700

    js 4 prakt 2.6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
+
         // $data = [
         //     'level_id' => 2,
         //     'username' => 'manager_tiga',
@@ -28,16 +29,16 @@ class UserController extends Controller
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
         // $user = UserModel::where('level_id',2)->count();
         // dd($user);
-        $user = UserModel::Create(
-            [
-                'username'=> 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ],
-        );
+        // $user = UserModel::Create(
+        //     [
+        //         'username'=> 'manager11',
+        //         'nama' => 'Manager11',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ],
+        // );
 
-        $user->username = 'manager11';
+        // $user->username = 'manager11';
         // $user->isDirty(); // true
         // $user->isDirty('username'); // true
         // $user->isDirty('nama'); // false
@@ -53,13 +54,58 @@ class UserController extends Controller
         // $user->isDirty(); // false
         // $user->isClean(); // true
 
+        // $user->save();
+
+        // $user->wasChanged(); // true
+        // $user->wasChanged('username'); // true
+        // $user->wasChanged(['username', 'nama']); // true
+        // $user->wasChanged('nama'); // false
+        // dd($user->wasChanged(['nama','username'])); // false
+        // return view('user', ['data' => $user]);
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id,
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
         $user->save();
 
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username', 'nama']); // true
-        $user->wasChanged('nama'); // false
-        dd($user->wasChanged(['nama','username'])); // false
-        // return view('user', ['data' => $user]);
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,26 +7,29 @@
         <h1>Data User</h1>
         <table border = "1" cellpadding= "2" cellspacing="0">
             <tr>
-                {{-- <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th> --}}
-                {{-- <th>ID Level Pengguna</th> --}}
-                <th>ID Level Pengguna</th>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>ID Level Pengguna</td>
+                <td>Aksi</td>
             </tr>
-            {{-- @foreach ($data as $d) --}}
+            @foreach ($data as $d)
             <tr>
                 {{-- <td>{{ $d->user_id }}</td>
                 <td>{{ $d->username}}</td>
                 <td>{{ $d->nama }}</td>
                 <td>{{ $d->level_id }}</td> --}}
 
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username}}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
-                {{-- <td>{{ $data }}</td> --}}
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username}}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td>
+                    <a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Delete</a>
+                </td>
+                
             </tr>
-            {{-- @endforeach --}}
+            @endforeach
         </table>
     </body>
 </html>
